9344549c6f18d02db28a0105dd84cf4bd90ee228
erika <erika24si@mahasiswa.pcr.ac.id>
2026-05-17 14:17:55 +0700

    Update project

--- src/layouts/project/AuthLayout.jsx
+++ src/layouts/project/AuthLayout.jsx
@@ -5,7 +5,7 @@ export default function AuthLayout() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#FEF6EE]">
       <div className="w-full max-w-md p-8 bg-white rounded-2xl shadow-xl">
-        <Outlet /> 
+        <Outlet />
       </div>
     </div>
   );
